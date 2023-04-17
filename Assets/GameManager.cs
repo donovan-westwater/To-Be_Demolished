65e39391b36f1e7e83dfa7a8bd4e7b40e9b2c5e9
donovan-westwater <donovan.westwater@gmail.com>
2023-04-16 17:38:45 -0700

    Zombie system seems to work on a basic level. Want to add the UI for game overs and restarts. Then work on the turret

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject[] spawnPoints;
+    public GameObject zombiePrefab;
+    public static GameManager instance;
+    float ctime = 0f;
+    float timer = 4f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ctime += Time.deltaTime;
+        if (ctime > timer)
+        {
+            ctime = 0;
+            zombiePrefab.SetActive(true);
+            foreach (GameObject s in spawnPoints)
+            {
+                GameObject.Instantiate(zombiePrefab, s.transform.position,s.transform.rotation);
+            }
+            zombiePrefab.SetActive(false);
+        }
     }
 }
--- Assets/ZombieAi.cs
+++ Assets/ZombieAi.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieAi : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health = 100f;
+    public float sightDist = 10f;
+    public GameObject testBuildingObj;
+    private NavMeshAgent zombieAgent;
+    private GameObject playerObj;
+    enum ZombieStates
     {
-        
+        Building,
+        Player
+    };
+    ZombieStates curState = ZombieStates.Building;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        zombieAgent = this.GetComponent<NavMeshAgent>();
+        zombieAgent.destination = testBuildingObj.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Vector3.Distance(playerObj.transform.position,this.transform.position) < sightDist)
+        {
+            RaycastHit hit;
+            Vector3 dir = playerObj.transform.position - this.transform.position;
+            if(Physics.Raycast(this.transform.position, dir.normalized, out hit, sightDist))
+            {
+                if (hit.collider.gameObject.tag.Equals("Player"))
+                {
+                    zombieAgent.destination = playerObj.transform.position;
+                    curState = ZombieStates.Player;
+                }
+                else
+                {
+                    zombieAgent.destination = testBuildingObj.transform.position;
+                    curState = ZombieStates.Building;
+                }
+            }
+        }
         
+        if (curState == ZombieStates.Building)
+        {
+            float d = Vector3.Distance(testBuildingObj.transform.position, this.transform.position);
+            if (d < 30f)
+            {
+                RaycastHit hit;
+                Vector3 dir = testBuildingObj.transform.position - this.transform.position;
+                if (Physics.Raycast(this.transform.position, dir.normalized, out hit, zombieAgent.radius+0.1f))
+                {
+                    Debug.Log("Attacking the Building");
+                }
+            }
+        }
+        
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag.Equals("Player"))
+        {
+            Debug.Log("Attacking the player!");
+        }
     }
 }
